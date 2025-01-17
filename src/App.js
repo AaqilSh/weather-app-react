2a63ffc91110e5ed5433aea5cec917b0cd3591b1
Aaqil Shihab <aaqilsh@yahoo.com>
2025-01-17 00:22:42 +0000

    Added temperature card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Header from "./components/Header";
 import TemperatureCard from "./components/TemperatureCard";
-
+import FeelsLikeCard from "./components/FeelsLikeCard";
 function App() {
   return (
     <div>
       <Header />
-      <div className="mt-8 flex justify-center">
+      <div className="mt-8 flex justify-center gap-8">
         <TemperatureCard temperature={25} />
+        <FeelsLikeCard feelsLike={25} />
       </div>
     </div>
   );

5d49b60cb0a55a7e06b6d5bca76aa5276656dbc1
Aaqil Shihab <aaqilsh@yahoo.com>
2025-01-28 19:16:01 +0000

    Layout changes

--- src/App.js
+++ src/App.js
@@ -12,22 +12,18 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-all duration-500">
       <Header className="mb-6 px-4" />
-      <div className="flex justify-center px-4 mt-4 mb-4">
+      <div className="justify-center px-4 mt-4 mb-4">
         <SearchBar />
       </div>
-      <div class="grid ">
-        <div class="grid">
-          <div className="px-4">
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
-              <TemperatureCard temperature={25} />
-              <FeelsLikeCard feelsLike={25} />
-              <HumidityCard humidity={90} />
-              <WindSpeedCard windSpeed={15} />
-              <UVIndexCard uvIndex={0} />
-              <SunriseSunsetCard sunrise={5} sunset={10} />
-              <AirQualityCard aqi={50} />
-            </div>
-          </div>
+      <div class="px-6 mt-8 ">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
+          <TemperatureCard temperature={25} />
+          <FeelsLikeCard feelsLike={25} />
+          <HumidityCard humidity={90} />
+          <WindSpeedCard windSpeed={15} />
+          <UVIndexCard uvIndex={0} />
+          <SunriseSunsetCard sunrise={5} sunset={10} />
+          <AirQualityCard aqi={50} />
         </div>
       </div>
     </div>

5888e8b2f8a3decb667cc8e49db356ce63a553bc
Aaqil Shihab <aaqilsh@yahoo.com>
2025-01-31 22:50:15 +0000

    HumudutyCard w96

--- src/components/HumidityCard.jsx
+++ src/components/HumidityCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HumidityCard = ({ humidity }) => {
   return (
-    <div className="w-60 bg-red-500 dark:bg-gray-800 shadow-md rounded-lg p-6 flex flex-col items-center justify-center text-white dark:text-gray-100 transition-all duration-500 ">
+    <div className="bg-red-500 dark:bg-gray-800 shadow-md rounded-lg p-6 flex flex-col items-center justify-center text-white dark:text-gray-100 transition-all duration-500 w-96 max-w-sm ">
       <div className="text-4xl mb-2">💧</div>
       <h2 className="text-xl font-bold mt-4">Humidity</h2>
       <p className="text-3xl font-semibold mt-2">{humidity}%</p>

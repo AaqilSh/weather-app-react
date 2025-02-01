309f17ec7d17e8a7f30e6f54f1af4991291e9024
Aaqil Shihab <aaqilsh@yahoo.com>
2025-02-01 15:42:08 +0000

    WindSpeedCard w96

--- src/components/WindSpeedCard.jsx
+++ src/components/WindSpeedCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const WindSpeedCard = ({ windSpeed }) => {
   return (
-    <div className="w-60 bg-blue-400 dark:bg-gray-800 shadow-md rounded-lg p-6 flex flex-col items-center justify-center text-white dark:text-gray-100 transition-all duration-500 ">
+    <div className=" bg-blue-400 dark:bg-gray-800 shadow-md rounded-lg p-6 flex flex-col items-center justify-center text-white dark:text-gray-100 transition-all duration-500 w-96 max-w-sm">
       <div className="text-4xl mb-2">🌬️</div>
       <h2 className="text-xl font-semibold mb-2">Wind Speed</h2>
       <p className="text-lg">{windSpeed} km/h</p>

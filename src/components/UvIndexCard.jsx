f89b2e3339fba151988fa1318f230356df289c53
Aaqil Shihab <aaqilsh@yahoo.com>
2025-02-02 00:22:43 +0000

    UVIndexCard w96

--- src/components/UvIndexCard.jsx
+++ src/components/UvIndexCard.jsx
@@ -13,7 +13,7 @@ const UVIndexCard = ({ uvIndex }) => {
   const uvLevel = getUVLevel(uvIndex);
 
   return (
-    <div className="w-60 bg-white dark:bg-gray-800 shadow-md rounded-lg p-6 flex flex-col items-center justify-center text-gray-900 dark:text-gray-100 transition-all duration-500">
+    <div className=" bg-white dark:bg-gray-800 shadow-md rounded-lg p-6 flex flex-col items-center justify-center text-gray-900 dark:text-gray-100 transition-all duration-500 w-96 max-w-sm">
       <div className="text-4xl mb-2">☀️</div>
       <h2 className="text-xl font-semibold mb-2">UV Index</h2>
       <p className="text-lg mb-1">Index: {uvIndex}</p>

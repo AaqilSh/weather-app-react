da4dd49f977dc00ce1cf3b40b02add98c7f42450
Aaqil Shihab <aaqilsh@yahoo.com>
2025-02-18 16:50:58 +0000

    Updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
 		}
 	  };
   return (
-    <header className="relative bg-blue-600 text-white px-6 py-4 flex items-center justify-between">
+    <header className="relative bg-blue-600  dark:bg-gray-800  text-white px-6 py-4 flex items-center justify-between">
 	<h1 className="text-2xl font-bold font-fredoka text-center w-full">Weathora 🌞</h1>
       <button onClick={toggleTheme} 
 	  className="absolute right-6 bg-white text-green-600 px-4 py-2 rounded-md shadow-md hover:bg-gray-200 transition-all duration-300">

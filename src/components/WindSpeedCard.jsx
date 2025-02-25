028e493e537071f713bdf92ae281705f41c67f5c
Aaqil Shihab <aaqilsh@yahoo.com>
2025-02-25 00:27:04 +0000

    Font size changed

--- src/components/WindSpeedCard.jsx
+++ src/components/WindSpeedCard.jsx
@@ -5,7 +5,7 @@ const WindSpeedCard = ({ windSpeed }) => {
     <div className=" bg-blue-400 dark:bg-gray-800 shadow-md rounded-lg p-6 flex flex-col items-center justify-center text-white dark:text-gray-100 transition-all duration-500 w-96 max-w-sm">
       <div className="text-4xl mb-2">🌬️</div>
       <h2 className="text-xl font-semibold mb-2">Wind Speed</h2>
-      <p className="text-lg">{windSpeed} km/h</p>
+      <p className="text-3xl font-semibold mt-2">{windSpeed} km/h</p>
     </div>
   );
 };
